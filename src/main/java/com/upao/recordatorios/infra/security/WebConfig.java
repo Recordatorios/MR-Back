7bf21d2b170e4b040647c8cb07e8507a6c869722
BMarin <bryanmarin2003@hotmail.com>
2024-07-05 19:32:55 -0500

    WebConfig configurado

--- src/main/java/com/upao/recordatorios/infra/security/WebConfig.java
+++ src/main/java/com/upao/recordatorios/infra/security/WebConfig.java
@@ -1,9 +1,24 @@
 package com.upao.recordatorios.infra.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-    // Any additional configuration if needed
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("Content-Type", "Authorization")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
